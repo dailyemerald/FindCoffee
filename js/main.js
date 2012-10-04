3140e83747c79afe8077bf9dc5c3e23f6d7eba17
Ivar Vong <ivarjvong@gmail.com>
2012-10-04 15:10:19 -0700

    add geo to leaflet to center map

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 // setup the map!
-window.map = L.map('map').setView([44.045131,-123.077673], 14);
+window.map = L.map('map');
 L.tileLayer('http://{s}.tile.cloudmade.com/03d6cb9913264bbbb3c57e37530f5b84/997/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
+    maxZoom: 18,
+    detectRetina: true
 }).addTo(window.map);
+
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+    window.user_location = e;
+    L.marker(e.latlng).addTo(map).bindPopup("You are here.").openPopup();
+    L.circle(e.latlng, radius).addTo(map);
+}
+window.map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+  window.user_location = null;
+  window.map.setView([44.045131,-123.077673], 14);
+}
+window.map.on('locationerror', onLocationError);
+
+window.map.locate({setView: true, maxZoom: 16});
 
 var compile_templates = function() {
   window.shopTpl = Handlebars.compile($("#shop-tpl").html());
 }
 
 var add_marker_to_map = function(coffeeshop) {
-  L.marker([coffeeshop.lat, coffeeshop.lng]).addTo(window.map).bindPopup(coffeeshop.name);
+  console.log("marker added:", L.marker([coffeeshop.lat, coffeeshop.lng]).addTo(window.map).bindPopup("<b>"+coffeeshop.name+"</b><br>"+coffeeshop.rating+"<br>"+coffeeshop.address));
+}
+
+var format_phone_number = function(phone) {
+  return "(" + phone.substring(0,3) + ") "+ phone.substring(3,6) + "-" + phone.substring(6,10);
 }
 
 var setup_coffee_shops = function() {
@@ -25,7 +46,7 @@ var setup_coffee_shops = function() {
       distance: "0 miles",
       lat: coffeeshop.location.coordinate.latitude,
       lng: coffeeshop.location.coordinate.longitude,
-      phone: coffeeshop.phone
+      phone: format_phone_number(coffeeshop.phone)
     }
     $("#coffee-shops").append( window.shopTpl(d) );
     
